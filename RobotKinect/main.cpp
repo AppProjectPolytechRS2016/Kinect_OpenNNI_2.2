cf4910c9eab000d226080aa1839f06d49aeba910
mikael10j <mikael.jacquemont@free.fr>
2015-01-31 17:58:45 +0100

    Initial Commit

---
+++ RobotKinect/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RobotKinect
+//
+//  Created by Mikael on 31/01/2015.
+//  Copyright (c) 2015 mikael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
